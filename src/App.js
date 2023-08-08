f35b2928334027396af83ebec0c09c1a95e770ad
Huyllee <moriarty1807@gmail.com>
2023-08-08 16:58:10 +0700

    design layout structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import "./App.css";
-import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  CryptoDetail,
+  Cryptocurrencies,
+  News,
+} from "./components";
+import "antd/dist/reset.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} exact />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetail />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserverd
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
