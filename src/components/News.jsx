a2b067a73420fb33f28fc86dc3bf89437086c28c
Huyllee <moriarty1807@gmail.com>
2023-08-08 23:28:27 +0700

    create news page

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,20 +1,85 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment/moment";
+import { Link } from "react-router-dom";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const defaultImage =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
 const News = ({ simplified = false }) => {
-  const count = simplified ? 10 : 100;
-  const { data: cryptoNews } = useGetCryptoNewsQuery({
+  const count = simplified ? 6 : 12;
+  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
     newsCategory: "Cryptocurrency",
     count,
   });
+  const ref = useRef("noreferrer");
+
+  if (isFetching) return "Loading..";
+
   console.log(cryptoNews);
 
-  return <div>News</div>;
+  return (
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => console.log(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          ></Select>
+        </Col>
+      )}
+      {cryptoNews?.value?.map((item, index) => (
+        <Col xs={24} sm={12} lg={8} key={index}>
+          <Card className="news-card" hoverable>
+            <a href={item.url} target="_blank" ref={ref}>
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {item.name}
+                </Title>
+                <img
+                  style={{ maxWidth: "200px", maxHeight: "100px" }}
+                  src={item?.image?.thumbnail?.contentUrl || defaultImage}
+                  alt="news"
+                />
+              </div>
+              <p>
+                {item.description.length > 100
+                  ? `${item.description.substring(0, 100)}...`
+                  : item.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      item.provider[0]?.image?.thumbnail?.contentUrl ||
+                      defaultImage
+                    }
+                    alt="news"
+                  />
+                  <Text className="provider-name">
+                    {item.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text>
+                  {moment(item.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
